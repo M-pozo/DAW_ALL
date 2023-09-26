013ce590162da8c173d22539c41cdebc1338310c
M-pozo <m.pozo.2002@gmail.com>
2023-09-26 20:37:50 +0200

    Cliente UD3 DAw

---
+++ DAW/UD3/Cliente/ud3.js
@@ -0,0 +1,14 @@
+'use strict'
+function miFuncion(a) {
+    let
+    potencia = 10;
+    return a * potencia; //Devuelve a multiplicado por 10
+}
+console.log(miFuncion('1e1'));
+
+let nano = 33 + false + '';
+console.log(nano);
+
+if (nano == 33){
+    console.log("padreada");
+}
