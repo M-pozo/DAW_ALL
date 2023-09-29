48b8ceb5791cf29470dc0f16a7220fa2d2d28afc
M-pozo <m.pozo.2002@gmail.com>
2023-09-29 19:13:07 +0200

    Actividades Porfolio

--- DAW/UD3/Porfolioapp/src/datos.php
+++ DAW/UD3/Porfolioapp/src/datos.php
@@ -1,40 +1,50 @@
 <?php
-//UD3.3.a BEGIN 
+//UD3.3.a and UD3.3.b BEGIN 
 $proyectos = [
     [
         "clave" => "proyecto1",
         "titulo" => "Porfolio",
         "descripcion" => "Mostrar todos mis proyectos",
-        "imagen" => ""
-        ],
+        "imagen" => "",
+        "fecha" => "24/09/2022",
+        "categorias" => ["php", "javascript"]
+    ],
     [
         "clave" => "proyecto2",
         "titulo" => "Torneo",
         "descripcion" => "Utilizar la API de un juego para coguer los datos de los participantes",
-        "imagen" => "static/images/torneo.png"
-        ],
+        "imagen" => "static/images/torneo.png",
+        "fecha" => "14/02/2023",
+        "categorias" => ["php", "javascript"]
+    ],
     [
         "clave" => "proyecto3",
         "titulo" => "MyFilms",
         "descripcion" => "Almacenar y categorizar peliculas segun mis reseñas",
-        "imagen" => "static/images/pelicula.png"
-        ],
+        "imagen" => "static/images/pelicula.png",
+        "fecha" => "12/11/2021",
+        "categorias" => ["php", "javascript"]
+    ],
     [
         "clave" => "proyecto4",
         "titulo" => "Juego 2D",
         "descripcion" => "Juego web 2D de plataformas",
-        "imagen" => "static/images/juego2d.png"
-        ],
+        "imagen" => "static/images/juego2d.png",
+        "fecha" => "29/12/2022",
+        "categorias" => ["php", "javascript"]
+    ],
     [
         "clave" => "proyecto5",
         "titulo" => "Ropa",
         "descripcion" => "Venta de ropa de segunda mano",
-        "imagen" => "static/images/ropa.png"
-        ],
+        "imagen" => "static/images/ropa.png",
+        "fecha" => "16/04/2020",
+        "categorias" => ["php", "javascript"]
+    ],
 ];
-//UD3.3.a END
+//UD3.3.a and UD3.3.b END
 
-$categoria = [
+$categorias = [
     "php" => "PHP",
     "javascript" => "JavaScript",
     "java" => "Java",
--- DAW/UD3/Porfolioapp/src/index.php
+++ DAW/UD3/Porfolioapp/src/index.php
@@ -2,29 +2,37 @@
 <?php include("datos.php"); ?>
 <?php include("utiles.php"); ?>
 <!--UD3.2.f BEGIN-->
-<a href="?sort=-1"><button href=""type="button" class="btn btn-outline-secondary">Descendente</button></a>
-<a href="?sort=1"><button href=""type="button" class="btn btn-outline-secondary">Ascendente</button></a>
+<a href="?sort=-1"><button href="" type="button" class="btn btn-outline-secondary">Descendente</button></a>
+<a href="?sort=1"><button href="" type="button" class="btn btn-outline-secondary">Ascendente</button></a>
 
 <?php
 $sort = $_GET['sort'];
-if(isset($_GET['sort']) && $_GET['sort'] == "-1"){
+if (isset($_GET['sort']) && $_GET['sort'] == "-1") {
     usort($proyectos, 'ordenaTituloProyectoDesc');
-}else{
+} else {
     usort($proyectos, 'ordenaTituloProyectoAsc');
 };
 ?>
 <!--UD3.2.f END-->
 <div class="container mb-5">
     <div class="row">
-        <?php foreach($proyectos as $proyecto): ?>
+        <?php foreach ($proyectos as $proyecto) : ?>
             <div class="col-sm-3">
-                <a href="#" class="p-5">
+                <!-- UD3.3.d-->
+                <a href="localhost/proyecto?id=<?php echo $proyecto["clave"] ?>" class="p-5">
                     <div clss="card">
                         <!-- UD3.2.c -->
-                        <img class="card-img-top" src="<?php echo $proyecto['imagen'] == '' ? 'static/images/default.png' : $proyecto['imagen']?>" alt="<?php echo $proyecto['titulo']?>">
+                        <img class="card-img-top" src="<?php echo $proyecto['imagen'] == '' ? 'static/images/default.png' : $proyecto['imagen'] ?>" alt="<?php echo $proyecto['titulo'] ?>">
                         <div class="card-body">
-                            <h5 class="card-title"><?php echo $proyecto['titulo']?></h5>
-                            <p class="card-text"><?php echo $proyecto['descripcion']?></p>
+                            <h5 class="card-title"><?php echo $proyecto['titulo'] ?></h5>
+                            <p class="card-text"><?php echo $proyecto['descripcion'] ?></p>
+                            <!-- UD3.3.c-->
+                            <p class="card-text">
+                                <?php foreach ($proyecto['categorias'] as $categoria) :
+                                    echo array_key_exists($categoria, $categorias) ? $categorias[$categoria] : "";
+                                endforeach; ?>
+                            </p>
+
                         </div>
                     </div>
                 </a>
