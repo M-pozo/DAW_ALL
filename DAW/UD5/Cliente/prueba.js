094f2658f65053d5fa7636df89874ff28438f10f
M-pozo <m.pozo.2002@gmail.com>
2023-11-15 19:16:12 +0100

    Cleinte

--- DAW/UD5/Cliente/prueba.js
+++ DAW/UD5/Cliente/prueba.js
@@ -1,5 +1,6 @@
 'use strict'
-while (true) {
-    let subVentana = window.open("https://www.google.es", "nueva", "height=600", "width=800")
-    subVentana.close();
-}
+
+let subVentana = window.open("https://www.google.es", "nueva", "height=600", "width=800")
+subVentana.close();
+
+window.location.assign("https://www.google.es")
