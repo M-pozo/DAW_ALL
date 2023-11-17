1a3e5e0e0bcf934f9e14ed4aaa1a96a5ac10b718
M-pozo <m.pozo.2002@gmail.com>
2023-11-17 17:49:29 +0100

    Avaluapp

--- DAW/UD6/Servidor/Proyecto/avaluaproy/avaluaproy/settings.py
+++ DAW/UD6/Servidor/Proyecto/avaluaproy/avaluaproy/settings.py
@@ -39,7 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'avaluaproyapp'
+    'avaluaproyapp',
+    'common',
+    'core',
+    'programacion_didactica',
+    'programacion_aula'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
