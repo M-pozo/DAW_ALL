45d1a0810cc5149c8a78cd321310adc38a6fe451
M-pozo <m.pozo.2002@gmail.com>
2023-11-17 20:40:43 +0100

    Cliente

--- DAW/UD5/Cliente/prueba.js
+++ DAW/UD5/Cliente/prueba.js
@@ -1,6 +1,27 @@
 'use strict'
 
-let subVentana = window.open("https://www.google.es", "nueva", "height=600", "width=800")
-subVentana.close();
+//let subVentana = window.open("https://www.google.es", "nueva", "height=600", "width=800")
+//subVentana.close();
 
-window.location.assign("https://www.google.es")
+//window.location.assign("https://www.google.es")
+
+document.write(navigator.appCodeName);
+document.write('<br>')
+document.write(navigator.appName);
+document.write('<br>')
+document.write(navigator.appVersion);
+document.write('<br>')
+document.write(navigator.cookieEnabled);
+document.write('<br>')
+document.write(navigator.platform);
+document.write('<br>')
+document.write(navigator.userAgent);
+
+document.write("Miguel");
+document.write("Pozo");
+document.title = "Pagina JS"
+
+let date = new Date(); // Crea objeto Date almacena la fecha actual
+console.log(typeof date); // Imprime object
+console.log(date instanceof Date); // Imprime true
+console.log(date); // Imprime fecha actual
