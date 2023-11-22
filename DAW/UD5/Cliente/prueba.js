e86184a7de65229f145aad04cce57c12800fcbdd
M-pozo <m.pozo.2002@gmail.com>
2023-11-22 19:07:51 +0100

    cliente

--- DAW/UD5/Cliente/prueba.js
+++ DAW/UD5/Cliente/prueba.js
@@ -5,7 +5,7 @@
 
 //window.location.assign("https://www.google.es")
 
-document.write(navigator.appCodeName);
+/*document.write(navigator.appCodeName);
 document.write('<br>')
 document.write(navigator.appName);
 document.write('<br>')
@@ -32,3 +32,25 @@ document.write("15236987K")
 let dateRgex = /^\d{2}\/\d{2}\/(\d{2}|\d{4})$/
 let telefonoRegex = /^\(\+([0-9]){3}\)(([0-9]{3})[-]){2}([0-9]){3}$/
 let gmailRegex = /^\w\@*.*$/
+*/
+
+let p2 = parseInt(prompt("De cuanto va a ser el salto?"))
+let p1 = prompt("Indique durante cuanto tiempo")
+let num = 0;
+let idInterval = setInterval(() => {
+    if (num > p1) { // Cuando imprimimos 10, paramos el timer para que no se repita más
+        window.alert("Trancurrio " + p1 + "s")
+        let continuar = prompt("Desea continuar S/N").toUpperCase()
+        if (continuar == "S") {
+            num = 0;
+            p2 = parseInt(prompt("De cuanto va a ser el salto?"))
+            p1 = prompt("Indique durante cuanto tiempo")
+        }else{
+            clearInterval(idInterval);
+            //window.location = "https://www.google.es"
+        }
+    }else{
+        console.log(num + "s trancurrido");
+        num = num + p2
+    }
+}, 1000)
