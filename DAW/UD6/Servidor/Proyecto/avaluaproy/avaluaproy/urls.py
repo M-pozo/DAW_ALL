b42a2640ccabfcca275969a6b58d552c9f18736b
M-pozo <117214588+M-pozo@users.noreply.github.com>
2023-11-24 10:36:19 +0100

    Avaluaproy

--- DAW/UD6/Servidor/Proyecto/avaluaproy/avaluaproy/urls.py
+++ DAW/UD6/Servidor/Proyecto/avaluaproy/avaluaproy/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for avaluaproy project.
+"""avaluapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,9 +18,32 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from common import views as common_views
+from core import views as core_views
+from programacion_didactica import views as programacion_didactica_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', common_views.HomeView.as_view(), name='home'),
+    path('panel/', common_views.PanelView.as_view(), name='panel'),
+    path('modulo_list/', core_views.ModuloListView.as_view(), name='modulo_list'),
+    path('modulo_detail/<int:pk>/', core_views.ModuloDetailView.as_view(), name='modulo_detail'),
+    path('ra_list/', core_views.RAListView.as_view(), name='ra_list'),
+    path('ra_detail/<int:pk>/', core_views.RADetailView.as_view(), name='ra_detail'),
+    path('ce_list/', core_views.CEListView.as_view(), name='ce_list'),
+    path('ce_detail/<int:pk>/', core_views.CEDetailView.as_view(), name='ce_detail'),
+    #UD6.7.b BEGIN
+    path('unidad_list/', programacion_didactica_views.UnidadListView.as_view(), name='unidad_list'),
+    path('unidad_detail/<int:pk>/', programacion_didactica_views.UnidadDetailView.as_view(), name='unidad_detail'),
+    path('ie_list/', programacion_didactica_views.InstEvListView.as_view(), name='ie_list'),
+    path('ie_detail/<int:pk>/', programacion_didactica_views.InstEvDetailView.as_view(), name='ie_detail'),
+    path('pond_ra_list/', programacion_didactica_views.PondRAListView.as_view(), name='pond_ra_list'),
+    path('pond_ra_detail/<int:pk>/', programacion_didactica_views.PondRADetailView.as_view(), name='pond_ra_detail'),
+    path('pond_ce_list/', programacion_didactica_views.PondCritListView.as_view(), name='pond_ce_list'),
+    path('pond_ce_detail/<int:pk>/', programacion_didactica_views.PondCritDetailView.as_view(), name='pond_ce_detail'),
+    path('pond_ce_ud_list/', programacion_didactica_views.PondCritUDListView.as_view(), name='pond_ce_ud_list'),
+    path('pond_ce_ud_detail/<int:pk>/', programacion_didactica_views.PondCritUDDetailView.as_view(), name='pond_ce_ud_detail'),
+    #UD6.7.b END
 ]
 
 if settings.DEBUG:
--- DAW/UD6/Servidor/Proyecto/avaluaproy/common/actividadorm.py
+++ DAW/UD6/Servidor/Proyecto/avaluaproy/common/actividadorm.py
@@ -1,5 +1,5 @@
 from core.models import Modulo, CritEvaluacion, ResAprendizaje
-from programacion_didactica.models import InstEvaluacion
+from programacion_didactica.models import InstEvaluacion, PondCritUD, PondCriterio, PondRA, Unidad
 
 #UD6.6 BEGIN
 #1
@@ -16,70 +16,91 @@ def get_crit_min():
 
 #4
 def get_ra_not_0612():
-    return ResAprendizaje.objects.filter(codigo__istartswith="0612")
-
+    return ResAprendizaje.objects.exclude(codigo__startswith='0612')
 #5
 def get_ie_I1_defensa():
-    return Modulo.objects.all()
+    return InstEvaluacion.objects.filter(codigo='I1', nombre__icontains='defensa')
 
 #6
 def get_mod_id_lt_2_cod_des():
-    return Modulo.objects.all()
+    return Modulo.objects.filter(id__lt=2).order_by('-codigo')
 
 #7
 def get_mod_id_gt_2():
-    return Modulo.objects.all()
+    return Modulo.objects.filter(id__gt=2)
 
 #8
 def get_mod_des_nom_des():
-    return Modulo.objects.all()
+    return Modulo.objects.filter(nombre__icontains='desarrollo').order_by('nombre')
 
 #9
 def get_ra_not_RA1():
-    return Modulo.objects.all()
+    return ResAprendizaje.objects.exclude(codigo__endswith='1')
 
 #10
 def get_ra_cli_RA3():
-    return Modulo.objects.all()
+    return ResAprendizaje.objects.get(modulo__icontains='cliente', codigo__endswith='3')
 
 #11
 def get_ce_RA06_not_cli():
-    return Modulo.objects.all()
+    return CritEvaluacion.objects.filter(resultado_aprendizaje__codigo__contains='06').exclude(modulo__nombre__icontains='cliente')
 
 #12
 def get_pon_ra_gt_5():
-    return Modulo.objects.all()
+    return PondRA.objects.filter(resultado_aprendizaje__porcentaje__gte=5)
 
 #13
 def get_ce_5_10_RA1():
-    return Modulo.objects.all()
+    return PondRA.objects.filter(
+        porcentaje__range=[5, 10],
+        criterio_evaluacion__resultado_aprendizaje__codigo__endswith='1',
+        criterio_evaluacion__resultado_aprendizaje__modulo__codigo='0613'
+    )
 
 #14
 def create_ie12():
-    return Modulo.objects.all()
+    return InstEvaluacion.objects.create(codigo='I12', nombre='Nuevo instrumento 12', descripcion='Nuevo instrumento 12')
 
 #15
 def create_ud15_pon():
-    return Modulo.objects.all()
+    ud15 = Unidad.objects.create(nombre='UD15', datos='Datos de tu elección')
+    ce = CritEvaluacion.objects.create(nombre='Criterio de evaluación de tu elección')
+    return PondRA.objects.create(unidad=ud15, criterio_evaluacion=ce, porcentaje=20)
 
 #16
 def update_last_ud():
-    return Modulo.objects.all()
+    last_ud = Unidad.objects.last()
+    last_ud.nombre += " (última)"
+    last_ud.save()
 
 #17
 def update_ce_RA2_x2():
-    return Modulo.objects.all()
+    ce_list = CritEvaluacion.objects.filter(
+        resultado_aprendizaje__codigo__endswith='2',
+        resultado_aprendizaje__modulo__codigo='0613'
+    )
+    for ce in ce_list:
+        ce.ponderacion *= 2
+        ce.save()
 
 #18
 def update_pon_ce_RA1_div2():
-    return Modulo.objects.all()
+    ce_list = CritEvaluacion.objects.filter(
+        resultado_aprendizaje__codigo__endswith='1',
+        resultado_aprendizaje__modulo__codigo='0613',
+        ponderacion__par=True
+    )
+    for ce in ce_list:
+        ce.ponderacion /= 2
+        ce.save()
 
 #19
 def delete_pon_ce_lt_5():
-    return Modulo.objects.all()
+    PondCriterio.objects.filter(porcentaje__lt=5).delete()
 
 #20
 def delete_pon_ce_UD15():
-    return Modulo.objects.all()
+    unidad = Unidad.objects.get(nombre__icontains='UD3')
+    PondCritUD.objects.filter(unidad=unidad).delete()
 #UD6.6 END
 
