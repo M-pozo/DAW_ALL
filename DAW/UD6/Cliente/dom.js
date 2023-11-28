b29348e098fe70ca94cd2c451ca4d1fb1de04fa1
M-pozo <m.pozo.2002@gmail.com>
2023-11-28 20:41:19 +0100

    Cliente

--- DAW/UD6/Cliente/dom.js
+++ DAW/UD6/Cliente/dom.js
@@ -13,3 +13,24 @@ console.log(elemento.nestElementSibling)
 console.log(elemento.previousElementSibling)
 
 //elemento.childNode[2] = "Hola <b>Baracola</b>"
+
+/*let nuevoElemento = document.createElement("div");
+nuevoElemento.textContent = "este es el nuevo elemento";
+let padre = document.getElementById("padre");
+padre.appendChild(nuevoElemento);*/
+
+let nuevoElemento = document.createElement("ol");
+let contador = parseInt(prompt("Cuantas filas quieres?"))
+for (let index = 1; index <= contador; index++) {
+    let li = document.createElement("li")
+    nuevoElemento.appendChild(li);
+    let contenido = prompt("Que le añadimos a la fila"+index)
+    li.textContent = contenido;
+    nuevoElemento.removeChild(li)
+}
+let actividad = document.getElementById("actividad");
+actividad.insertBefore(nuevoElemento, actividad.children[1]);
+
+let div3 = document.getElementById("3");
+div3.remove();
+
