6677082ea061e2e652a9f8ec5bacda4dff2d1d21
M-pozo <m.pozo.2002@gmail.com>
2023-11-29 19:08:53 +0100

    Cliente

--- DAW/UD6/Cliente/dom.js
+++ DAW/UD6/Cliente/dom.js
@@ -19,7 +19,7 @@ nuevoElemento.textContent = "este es el nuevo elemento";
 let padre = document.getElementById("padre");
 padre.appendChild(nuevoElemento);*/
 
-let nuevoElemento = document.createElement("ol");
+/*let nuevoElemento = document.createElement("ol");
 let contador = parseInt(prompt("Cuantas filas quieres?"))
 for (let index = 1; index <= contador; index++) {
     let li = document.createElement("li")
@@ -32,5 +32,49 @@ let actividad = document.getElementById("actividad");
 actividad.insertBefore(nuevoElemento, actividad.children[1]);
 
 let div3 = document.getElementById("3");
-div3.remove();
+div3.remove();*/
 
+/*let actividad2 = document.getElementById("actividad2")
+for (let index = 1; index <= 10; index++) {
+    let div = document.createElement("div")
+    actividad2.appendChild(div)
+    div.setAttribute("id", index)
+    div.textContent = "Este es el div hijo "+index
+}
+let seleccionDiv = parseInt(prompt("A que div quieres añadirle contenido"))
+let divSeleccionado = actividad2.children[seleccionDiv-1]
+let contenidoDiv = prompt("Que contenido quieres añadir?")
+divSeleccionado.textContent = contenidoDiv*/
+let actividad3 = document.getElementById("actividad3")
+function crearDivs(color) {
+    for (let index = 0; index < 255; index++) {
+        let div = document.createElement("div")
+        actividad3.appendChild(div)
+        div.style.width = "100%"
+        div.style.height = "2px"
+        if (color == "gris") { 
+            div.style.backgroundColor = "rgb(" + index + ", " + index + ", " + index + ")" 
+        } else if (color == "verde") {
+            div.style.backgroundColor = "rgb(000, " + index + ", 000)"
+        } else if (color == "rojo") {
+            div.style.backgroundColor = "rgb(" + index + ", 000, 000)"
+        };
+    }
+}
+let resultado = parseInt(prompt(`Que color quieres? 
+    1. Grises
+    2. verde
+    3. rojo`));
+switch (resultado) {
+    case 1:
+        crearDivs("gris")
+        break;
+    case 2:
+        crearDivs("verde")
+        break;
+    case 3:
+        crearDivs("rojo")
+        break;
+    default:
+        break;
+}
