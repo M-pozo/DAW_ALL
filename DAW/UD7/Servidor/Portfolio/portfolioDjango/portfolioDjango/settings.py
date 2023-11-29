5f53d235aeb030148d621c49b2eafe2aca76bd8d
M-pozo <m.pozo.2002@gmail.com>
2023-11-29 16:49:19 +0100

    Portfolio

--- DAW/UD7/Servidor/Portfolio/portfolioDjango/portfolioDjango/settings.py
+++ DAW/UD7/Servidor/Portfolio/portfolioDjango/portfolioDjango/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     'portfolioapp',
-    'notificaciones'
+    'notificaciones',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +132,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MESSAGE_TAGS = {messages.ERROR: 'danger'}
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
