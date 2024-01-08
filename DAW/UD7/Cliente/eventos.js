4536b03cfdcab27c92fc1b8dff4047e6a9c26424
M-pozo <m.pozo.2002@gmail.com>
2024-01-08 19:06:09 +0100

    DAW Cliente

--- DAW/UD7/Cliente/eventos.js
+++ DAW/UD7/Cliente/eventos.js
@@ -1,10 +1,30 @@
 'use strict'
-let contador = document.getElementById("contador")
+let elemento = document.getElementById("contador")
 
-function inputClick(elemento, even) {
-    let a = 0
-    let p = contador.createElement("p")
-    p.textContent = parseInt(a+1)  
+function inputClick(event) {
+    let iz=document.getElementById("iz")
+    let dr = document.getElementById("dr")
+    if (event.button == 0) {
+        document.write("fadsfa")
+        if (iz == undefined) {
+            iz = document.createElement("div")
+            iz.style.float = "right"
+            iz.textContent = "1"
+            iz.id = "iz"
+            iz.body.appendChild(iz)
+        } else {
+            iz.textContent = (parseInt(iz.textContent) + 1)
+        }
+    } else if (dr == undefined){
+        if (dr == undefined) {
+            dr = document.createElement("div")
+            dr.style.float = "right"
+            dr.textContent = "1"
+            dr.id = "dr"
+            document.body.appendChild(dr)
+        } else {
+            dr.textContent = (parseInt(dr.textContent) + 1)
+        }
+    }
 }
 
-contador.addEventListener('click', console.log).inputClick
