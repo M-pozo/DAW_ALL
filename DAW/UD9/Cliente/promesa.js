61eca2dacb0a2102193c6cc27fe05c6286283776
M-pozo <m.pozo.2002@gmail.com>
2024-01-30 20:31:24 +0100

    Cliente

--- DAW/UD9/Cliente/promesa.js
+++ DAW/UD9/Cliente/promesa.js
@@ -21,10 +21,43 @@ function llamadaApi(url) {
 
 //En la variable personaje tenemos una promesa, que internamente ha hecho una llamada
 //a una API con el objeto XMLHttpRequest
-let personaje = llamadaApi("https://swapi.dev/api/people/1/");
+//let personaje = llamadaApi("https://swapi.dev/api/people/1/");
 //Ahora podemos consumir la promesa
-personaje
+/*personaje
     .then(dato => console.log(dato.name))
-    .catch(error => console.log(error));
+    .catch(error => console.log(error))
 
-console.log("🇾🇪")
+.then(personaje=>{
+    console.log(personaje);
+    console.log(personaje.name);
+    personaje.films.forEach(peli=>{
+        llamadaApi(api)
+        .then(json_peli=>console.log(json_peli.title))
+    })
+
+    let arrayDePromesa=personaje.films.map(peli=>llamadaApi(peli));
+    return Promise.all(arrayDePromesa);
+})
+.catch(error=>console.log("catch: "+error));
+
+console.log("Después de la promesa")
+*/
+//personaje peliculas > planetas
+
+let personajes = llamadaApi("https://swapi.dev/api/people/1")
+personajes.then(personaje => {
+    console.log(personaje.name)
+    personaje.films.forEach(pelicula => {
+        let peliculas = llamadaApi(pelicula)
+        peliculas.then(pelicula => {
+            console.log(pelicula.title)
+            pelicula.planets.forEach(planeta => {
+                let planetas = llamadaApi(planeta)
+                planetas.then(planet => console.log(planet.name))
+            })
+
+        })
+    })
+
+})
+.catch(error => console.log(error));
