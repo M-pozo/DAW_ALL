125bbf77ad9a29d3d740c7d394a4694547d35921
M-pozo <m.pozo.2002@gmail.com>
2024-01-31 19:13:46 +0100

    Cliente

---
+++ DAW/UD7/Cliente/Actividad/eventos.js
@@ -0,0 +1,49 @@
+'use strict'
+/*
+//UD7 Tarea 1
+// Crear div y añadir stilos
+let contenedor = document.createElement("div");
+contenedor.style.height = "100px";
+contenedor.style.width = "100px";
+contenedor.style.backgroundColor = "black";
+// Crear el parrafo del div para el mensaje
+let parrafo = document.createElement("p");
+parrafo.textContent = "Pulsa aquí";
+parrafo.style.color = "white";
+
+//Añadir los elementos al documento
+contenedor.appendChild(parrafo);
+document.body.appendChild(contenedor);
+
+contenedor.addEventListener("click", function () {
+    window.alert("Pulsaste sobre el div");
+})
+*/
+
+//UD7 Tarea 2
+let div1 = document.createElement("div");
+let contenidoDiv1 = document.createElement("p");
+contenidoDiv1.textContent = "Pasa por aqui";
+div1.style.height = "100px";
+div1.style.width = "100px";
+div1.appendChild(contenidoDiv1);
+document.body.appendChild(div1);
+
+let div2 = document.createElement("div");
+div2.id = "efecto"
+let contenidoDiv2 = document.createElement("p");
+contenidoDiv2.textContent = "Efectos del movimiento";
+div2.style.backgroundColor = "red";
+div2.style.height = "100px";
+div2.style.width = "100px";
+document.body.appendChild(div2);
+//[div1, div2].map(d => document.body.appendChild(d))
+
+div1.addEventListener("mouseover", function () {
+    div2.style.backgroundColor = "green";
+})
+
+
+
+
+
