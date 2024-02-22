719b88eee9ba2380fdf6dd68208a7bfcfd6b5083
M-pozo <117214588+M-pozo@users.noreply.github.com>
2024-02-22 14:01:15 +0100

    Cliente

--- DAW/UD9/Cliente/Actividad/assets/js/avaluapp.js
+++ DAW/UD9/Cliente/Actividad/assets/js/avaluapp.js
@@ -4,11 +4,19 @@ function limpiarTablas() {
     
     contenedores.forEach(contenedor => {
         const tabla = contenedor.querySelector('table');
+        const button = contenedor.querySelector('button');
+        const form = contenedor.querySelector('form');
         if (tabla) {
             contenedor.removeChild(tabla);
+        }if (form) {
+            contenedor.removeChild(form);
+            contenedor.removeChild(button);
+        }if (button) {
+            contenedor.removeChild(button);
         }
     });
 }
+
 //Funciones para esconder y mostrar contenedores
 function ocultarContenedor(className) {
     const contendor = document.getElementsByClassName(className);
@@ -81,9 +89,7 @@ function crearTabla(list) {
     fetch('http://localhost:8000/api/'+list+'/', get)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         const ra = data.results; // Obtener la lista de módulos de los resultados
-        console.log(ra)
         // Crear una tabla HTML
         const tabla = document.createElement('table');
         
@@ -110,17 +116,23 @@ function crearTabla(list) {
                 mostrarDetalles(list, valor.id);
             });
         });
+                
+        const button = document.createElement('button')
+        button.textContent = 'Crear'
+        button.addEventListener('click', () => {
+            createForm(list);
+        });
         
-        // Agregar la tabla al elemento contenedor en el HTML
         if (list == 'ra_list') {
             ra_list.appendChild(tabla);
+            ra_list.appendChild(button);
         }else if (list == 'modulo_list'){
             modulo_list.appendChild(tabla);
+            modulo_list.appendChild(button);
         }else if (list == 'ce_list'){
             ce_list.appendChild(tabla);
+            ce_list.appendChild(button);
         }
-        
-        
     })
     .catch(error => {
         console.error('Error al obtener la lista de módulos:', error);
@@ -128,15 +140,12 @@ function crearTabla(list) {
 }
 
 document.getElementById('ra').addEventListener('click', function(event) {
-    event.preventDefault();
     crearTabla('ra_list')
 })
 document.getElementById('modulo').addEventListener('click', function(event) {
-    event.preventDefault();
     crearTabla('modulo_list')
 })
 document.getElementById('ce').addEventListener('click', function(event) {
-    event.preventDefault();
     crearTabla('ce_list')
 })
 
@@ -160,7 +169,6 @@ function mostrarDetalles(list, valorId) {
     fetch('http://localhost:8000/api/'+detail+'/'+valorId+'/', get)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
 
         // Crear una tabla HTML
         const tabla = document.createElement('table');
@@ -219,11 +227,228 @@ function mostrarDetalles(list, valorId) {
         
 
         // Agregar la tabla al elemento contenedor en el HTML
+        const button = document.createElement('button')
+        button.textContent = 'Eliminar'
+        button.id = 'delete'
+        button.addEventListener('click', () => {
+            eliminarRegistro(list, valorId);
+        });
+
+        const button2 = document.createElement('button')
+        button2.textContent = 'Actualizar'
+        button2.id = 'actualizar'
+        button2.addEventListener('click', () => {
+            actualizarRegistro(list, valorId);
+        });
+
         modulo_detail.appendChild(tabla);
+        modulo_detail.appendChild(button);
+        modulo_detail.appendChild(button2);
     })
     .catch(error => {
         console.error('Error al obtener los detalles del módulo:', error);
     });
 }
 
+function createForm(list) {
+    limpiarTablas()
+    let data;
+    let formFields = [];
 
+    if (list === 'modulo_list') {
+        data = {
+            codigo: 'email',
+            nombre: 'password',
+        };
+    } else if (list === 'ra_list') {
+        data = {
+            codigo: 'email',
+            descripcion: 'password',
+            modulo: 'password',
+        };
+    } else if (list === 'ce_list') {
+        data = {
+            codigo: 'email',
+            descripcion: 'password',
+            minimo: 'email',
+            resultado_aprendizaje: 'password',
+        };
+    }
+
+    // Crear un array de campos de formulario
+    for (let key in data) {
+        formFields.push({ name: key, type: 'text'});
+    }
+
+    // Crear el formulario
+    const form = document.createElement('form');
+    form.setAttribute('id', `${list}_form`);
+
+    // Crear los campos de formulario
+    formFields.forEach(field => {
+        const label = document.createElement('label');
+        label.textContent = `${field.name}: `;
+        const input = document.createElement('input');
+        input.setAttribute('type', field.type);
+        input.setAttribute('name', field.name);
+        input.setAttribute('required', true); // Marcar como requerido
+        form.appendChild(label);
+        form.appendChild(input);
+        form.appendChild(document.createElement('br'));
+    });
+
+    // Agregar un botón de envío
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Enviar';
+    form.appendChild(submitButton);
+
+    // Agregar el formulario al documento
+    if (list == 'ra_list') {
+        ra_list.appendChild(form);
+    }else if (list == 'modulo_list'){
+        modulo_list.appendChild(form);
+    }else if (list == 'ce_list'){
+        ce_list.appendChild(form);
+    }
+
+    document.getElementById(`${list}_form`).addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar que se envíe el formulario de forma tradicional
+    
+        // Obtener los valores de los campos del formulario
+        const formData = new FormData(this);
+        const data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+        console.log(data)
+        // Construir la solicitud POST
+        const token = localStorage.getItem("token");
+        const post = {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        };
+    
+        // Enviar la solicitud POST al endpoint correspondiente
+        let detail
+        if (list == 'ra_list') {
+            detail = 'ra_detail'
+        }else if (list == 'modulo_list'){
+            detail = 'modulo_detail'
+        }else if (list == 'ce_list'){
+            detail = 'ce_detail'
+        }
+        fetch('http://localhost:8000/api/'+detail+'/', post)
+        limpiarTablas()
+    });
+    
+}
+
+function eliminarRegistro(list, valorId) {
+    limpiarTablas();
+    var token = localStorage.getItem("token");
+    const eliminar = {
+        method: 'DELETE',
+        headers: {
+            Authorization: 'Bearer ' + token
+        }
+    };
+    let detail
+    if (list == 'ra_list') {
+        detail = 'ra_detail'
+    }else if (list == 'modulo_list'){
+        detail = 'modulo_detail'
+    }else if (list == 'ce_list'){
+        detail = 'ce_detail'
+    }
+    fetch('http://localhost:8000/api/'+detail+'/'+valorId+'/', eliminar)
+}
+
+/*function actualizarRegistro(list, valorId) {
+    limpiarTablas()
+    let data;
+    let formFields = [];
+
+    if (list === 'modulo_list') {
+        data = {
+            codigo: 'email',
+            nombre: 'password',
+        };
+    } else if (list === 'ra_list') {
+        data = {
+            codigo: 'email',
+            descripcion: 'password',
+            modulo: 'password',
+        };
+    } else if (list === 'ce_list') {
+        data = {
+            codigo: 'email',
+            descripcion: 'password',
+            minimo: 'email',
+            resultado_aprendizaje: 'password',
+        };
+    }
+
+    // Crear un array de campos de formulario
+    for (let key in data) {
+        formFields.push({ name: key, type: 'text'});
+    }
+
+    // Crear el formulario
+    const form = document.createElement('form');
+    form.setAttribute('id', `${list}_form`);
+
+    // Crear los campos de formulario
+    formFields.forEach(field => {
+        const label = document.createElement('label');
+        label.textContent = `${field.name}: `;
+        const input = document.createElement('input');
+        input.setAttribute('type', field.type);
+        input.setAttribute('name', field.name);
+        input.setAttribute('required', true); // Marcar como requerido
+        form.appendChild(label);
+        form.appendChild(input);
+        form.appendChild(document.createElement('br'));
+    });
+
+    // Agregar un botón de envío
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Enviar';
+    form.appendChild(submitButton);
+
+    // Agregar el formulario al documento
+    if (list == 'ra_list') {
+        ra_list.appendChild(form);
+    }else if (list == 'modulo_list'){
+        modulo_list.appendChild(form);
+    }else if (list == 'ce_list'){
+        ce_list.appendChild(form);
+    }
+
+    document.getElementById(`${list}_form`).addEventListener('submit', function(event) {
+        event.preventDefault(); // Evitar que se envíe el formulario de forma tradicional
+        const formData = new FormData(this);
+        const data = {};
+        formData.forEach((value, key) => {
+            data[key] = value;
+        });
+    
+        console.log(data)
+        // Construir la solicitud PUT o PATCH
+        const token = localStorage.getItem("token");
+        const update = {
+            method: 'PUT', // O 'PATCH' dependiendo de tu API
+            headers: {
+                'Authorization': 'Bearer ' + token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        };
+    
+        // Enviar la solicitud PUT o PATCH al endpoint correspondiente
+        fetch(`http://localhost:8000/api/${list}/${valorId}/`, update)
+    });
+}*/
